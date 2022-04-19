dbba34ac2e7a7cb77b34186f948afe4b576d550e
OlhaKhodorenko <khodorenkoo@gmail.com>
2022-04-19 15:09:51 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,16 @@
+const form = document.querySelector(".login-form");
 
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+  console.log(`Login: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+};
+form.addEventListener("submit", handleSubmit);
