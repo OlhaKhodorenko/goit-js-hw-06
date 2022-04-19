dd6d642d6a5d245060a4041400fdd4160ef7112f
OlhaKhodorenko <khodorenkoo@gmail.com>
2022-04-19 15:09:56 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
+
+const bodyEl = document.querySelector("body");
+const colorBtn = document.querySelector(".change-color");
+const descrColor = document.querySelector(".color");
+
+const changeColor = () =>{
+  const newColor = getRandomHexColor()
+  bodyEl.style.backgroundColor = newColor;
+  descrColor.textContent = newColor;
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+colorBtn.addEventListener("click", changeColor);
