c14912d2f70e0533e660f00f50708b2148993906
OlhaKhodorenko <khodorenkoo@gmail.com>
2022-04-19 15:09:29 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,10 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+ const ingredientsItems = ingredients.map(el => {const itemList = document.createElement("li");
+ itemList.textContent = el;
+ itemList.classList.add("item");
+ return itemList;
+});
+const ingredientsList = document.querySelector("#ingredients").append(...ingredientsItems);
